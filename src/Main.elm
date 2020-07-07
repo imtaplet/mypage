8e2e108656e83a8cf5e9dc00319ed9d8242cef26
imtaplet <rockgajoen@gmail.com>
2020-07-07 23:41:25 +0900

    add profile

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,38 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (..)
 
 
+viewProfile : Html msg
+viewProfile =
+    text """
+こんばんは。imtaplet(いんたぷれっと)と申します‼
+私のマイページにアクセスしていただいて、ありがとうございます。
+ここには、私の学習記録や日記などを書いていく予定です。
+
+ノートパソコンひとつで食べていけたらいいなと思って、プログラミングを勉強しています。でも私はメンタルが弱くて社会性はあまりないので、半分叶わない夢だと諦めています。
+"""
+
+
+viewHeader : Html msg
+viewHeader =
+    header []
+        [ h1 [] [ text "imtaplet's mypage" ]
+        ]
+
+
+main : Html msg
 main =
-    text "imtaplet's mypage"
+    div []
+        [ viewHeader
+        , h2 [] [ text "自己紹介" ]
+        , p [] [ viewProfile ]
+        , dl []
+            [ dt [] [ text "趣味" ]
+            , dt [] [ text "チャットやTwitterなどのテキストコミュニケーションが趣味です。" ]
+            , dt [] [ text "仕事" ]
+            , dt [] [ text "現在無職で、求職中です。" ]
+            ]
+        , h2 [] [ text "日記" ]
+        , p [] [ text "まだ日記はありません。" ]
+        ]
