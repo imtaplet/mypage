7a20bda8b4b83c9ea47761867f77078093b0402c
imtaplet <rockgajoen@gmail.com>
2020-07-30 11:04:00 +0900

    Set margin for p elements.

--- src/GlobalCss.elm
+++ src/GlobalCss.elm
@@ -1,13 +1,15 @@
 module GlobalCss exposing (snippets)
 
 import Css exposing (..)
-import Css.Global exposing (..)
+import Css.Global exposing (Snippet, children, typeSelector)
 
 
 snippets : List Snippet
 snippets =
     [ typeSelector "h2"
         [ backgroundColor (hex "204051") ]
+    , typeSelector "p"
+        [ margin2 (em 1.0) (px 0) ]
     , typeSelector "pre"
         [ children
             [ typeSelector "code"
