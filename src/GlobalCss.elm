04b302b14eea9eef14afa33e48cc9682f770a025
imtaplet <rockgajoen@gmail.com>
2020-07-30 11:32:12 +0900

    Style list elements.

--- src/GlobalCss.elm
+++ src/GlobalCss.elm
@@ -4,12 +4,21 @@ import Css exposing (..)
 import Css.Global exposing (Snippet, children, typeSelector)
 
 
+paddingInlineStart : Length compatible units -> Style
+paddingInlineStart arg1 =
+    property "padding-inline-start" arg1.value
+
+
 snippets : List Snippet
 snippets =
     [ typeSelector "h2"
         [ backgroundColor (hex "204051") ]
     , typeSelector "p"
         [ margin2 (em 1.0) (px 0) ]
+    , typeSelector "ol"
+        [ paddingInlineStart (em 1.0) ]
+    , typeSelector "ul"
+        [ paddingInlineStart (em 1.0) ]
     , typeSelector "pre"
         [ children
             [ typeSelector "code"
