b6985a1962aa278be3b4748db999c5ec43550cf3
imtaplet <rockgajoen@gmail.com>
2020-08-03 23:54:12 +0900

    Adjust font-size of h2 in the main visual.

--- src/TopImage.elm
+++ src/TopImage.elm
@@ -2,6 +2,7 @@ module TopImage exposing (view)
 
 import ColorScheme exposing (..)
 import Css exposing (..)
+import Css.Media exposing (only, screen, withMedia)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css)
 
@@ -9,7 +10,16 @@ import Html.Styled.Attributes exposing (css)
 slogan : Html msg
 slogan =
     h2
-        []
+        [ css
+            ([ fontSize (vw 5)
+             ]
+                ++ [ withMedia
+                        [ only screen [ Css.Media.minWidth (px 480) ] ]
+                        [ fontSize (vw 2.4)
+                        ]
+                   ]
+            )
+        ]
         [ text "今日も明日も曇りで憂鬱💦"
         , br [] []
         , text "マイスリーをモンエナで迎えよう😢"
